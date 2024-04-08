abfa3f6d95789f4b46ab6d33c75ea61316d664fe
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-08 11:45:51 +0300

    Log primitive data types

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert('message');
+// alert(77);
+//comment oneline
+// alert(true);
+/*
+ manyline 
+ comment
+*/
+
+console.log('ten');        //string
+console.log(18.45);        //number
+console.log(10n);          //bigint
+console.log(true);         //boolean
+console.log(undefined);    //undefined
+console.log(null);         //object
