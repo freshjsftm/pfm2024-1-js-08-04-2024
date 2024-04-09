28771a39a5a7a756abd12acbd40b32ab93322bce
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-09 12:10:12 +0300

    Add example explicit and implicit data type casting

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-// console.log('ageAnimal = ', ageAnimal);
-// console.log('type ageAnimal = ',typeof ageAnimal);
+const numberFromDB = 4;
+const numberFromUser = '11';
+const conversionNumberFromUser =  Number(numberFromUser);
+const result = numberFromDB +  conversionNumberFromUser;
+console.log('result =', result);
+console.log('typeof result =', typeof result);
+console.log('typeof conversionNumberFromUser =', typeof conversionNumberFromUser);
 
-let ageAnimal;
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
-ageAnimal = 2;
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
-ageAnimal = 'one';
-console.log('ageAnimal = ', ageAnimal);
-console.log('type ageAnimal = ', typeof ageAnimal);
+const isActiveUser = false;
+const result2 = numberFromDB + Number(isActiveUser);
+console.log('result =', result2);
+console.log('typeof result =', typeof result2);
 
-const amountPawsAnimal = 4;
-const AMOUNT_DAYS_WEEK = 7;
-
-// створіть змінні для:
-// - ім'я користувача (Маша)
-const userFirstName = 'Маша';
-// - вік користувача (23)
-const userAge = 23;
-// - назва мови програмування (С++)
-const nameProgrammingLanguage = 'C++';
-// - чи повнолітній користувач  (true)
-const isAdultUser = true;
-// - користувач громодянство України (null)
-const citizenshipUkraine = null;
-// - рік народження користувача (undefined)
-let userYearBirthday;
-
-console.log('title book: "qwerty" ');
-console.log("I'm a teacher");
-console.log("I'm a teacher");
-
-const login = 'qwerty';
-
-{
-  const login = 'asdfgh';
-  console.log('login = ', login);
-}
-{
-  const login = 'wwww';
-  console.log('login = ', login);
-}
-{
-  const login = 'eeeee';
-  console.log('login = ', login);
-}
-
-console.log('login = ', login);
+const userFirstName = 'Bred';
+const userLastName = 'Pitt';
+const userFullName = userFirstName + ' ' + userLastName;
+console.log('userFullName =', userFullName);
