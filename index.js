cf4af2d53c29a80290bb27956f552ef7bdd91298
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-09 11:05:43 +0300

    Add example declaration variables

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// alert('message');
-// alert(77);
-//comment oneline
-// alert(true);
-/*
- manyline 
- comment
-*/
+// console.log('ageAnimal = ', ageAnimal);
+// console.log('type ageAnimal = ',typeof ageAnimal);
 
-console.log('ten');        //string
-console.log(18.45);        //number
-console.log(10n);          //bigint
-console.log(true);         //boolean
-console.log(undefined);    //undefined
-console.log(null);         //object
+let ageAnimal;
+console.log('ageAnimal = ', ageAnimal);
+console.log('type ageAnimal = ', typeof ageAnimal);
+ageAnimal = 2;
+console.log('ageAnimal = ', ageAnimal);
+console.log('type ageAnimal = ', typeof ageAnimal);
+ageAnimal = 'one';
+console.log('ageAnimal = ', ageAnimal);
+console.log('type ageAnimal = ', typeof ageAnimal);
+
+const amountPawsAnimal = 4;
+const AMOUNT_DAYS_WEEK = 7;
+
+// створіть змінні для:
+// - ім'я користувача (Маша)
+const userFirstName = 'Маша';
+// - вік користувача (23)
+const userAge = 23;
+// - назва мови програмування (С++)
+const nameProgrammingLanguage = 'C++';
+// - чи повнолітній користувач  (true)
+const isAdultUser = true;
+// - користувач громодянство України (null)
+const citizenshipUkraine = null;
+// - рік народження користувача (undefined)
+let userYearBirthday;
+
+console.log('title book: "qwerty" ');
+console.log("I'm a teacher");
+console.log("I'm a teacher");
+
+const login = 'qwerty';
+
+{
+  const login = 'asdfgh';
+  console.log('login = ', login);
+}
+{
+  const login = 'wwww';
+  console.log('login = ', login);
+}
+{
+  const login = 'eeeee';
+  console.log('login = ', login);
+}
+
+console.log('login = ', login);
