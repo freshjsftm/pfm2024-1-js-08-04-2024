2edb9ebdee654819e4f6de4a2be2c7e07a791092
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 10:37:28 +0300

    Add example any assignment

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+let number1 = 10;
+const number2 = 4;
 
+//number1 = number1 + number2;
+number1 += number2;
 
-const base1 = 2;
-const pow1 = 3;
-const result1 = base1 ** pow1;
-console.log('result1', result1);
+//number1 = number1 / 3;
+number1 /= 3;
 
-const base2 = 2;
-const pow2 = -3;
-const result2 = base2 ** pow2;
-console.log('result2', result2);
+//number1 = number1 * number2;
+number1 *= number2;
 
-const base3 = 2;
-const pow3 = 3;
-const result3 = (-base3) ** pow3;
-console.log('result3', result3);
+//number1 = number1 ** 2;
+number1 **= 2;
 
+//number1 = number1 % 5
+number1 %= 5;
 
+//number1 = number1 - 1;
+number1--;
 
-
+number1 = (number1 + 2) * 3 + number1;
