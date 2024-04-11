ff7cb7c265044949b05ad38febce0dfa6afb341b
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 11:48:37 +0300

    Add logical NOT

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let number1 = 10;
-let number2 = '10';
 
-console.log(number1 === number2); //false
-console.log(number1 !== number2); //true
+const isAdult = true;
+
+// !  NOT
+
+const resultNot = !isAdult;
+
+console.log(resultNot);
+
+const userAge = 23;  //true
+console.log(!userAge);
+
+/*
+false
+0 -0 +0
+''
+undefined
+null
+NaN
+*/
+
+
+let quantity = 15;
+const isExists = !!quantity;
+
+
+
+
+
+
