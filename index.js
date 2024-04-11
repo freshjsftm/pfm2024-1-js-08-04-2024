337061bbc0b20428f275e2b7396d7c26a51915ce
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 11:17:14 +0300

    Add example relational

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let number1 = 10;
-const number2 = 4;
+let number2 = 10;
 
-//number1 = number1 + number2;
-number1 += number2;
+const resultLess = number1 < number2;
+console.log(resultLess);
+console.log(number1 >= number2);
 
-//number1 = number1 / 3;
-number1 /= 3;
+let str1 = 'cat';
+let str2 = 'category';
+let str3 = '102';
+let str4 = '2';
 
-//number1 = number1 * number2;
-number1 *= number2;
-
-//number1 = number1 ** 2;
-number1 **= 2;
-
-//number1 = number1 % 5
-number1 %= 5;
-
-//number1 = number1 - 1;
-number1--;
-
-number1 = (number1 + 2) * 3 + number1;
+console.log(str1 > str2);
+console.log(number1 > str3);
+console.log(number1 > true);
+console.log(number1 > undefined);
+console.log(number1 > null);
+console.log(number1 > NaN);
+console.log(str1 > NaN);
