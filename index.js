34a6c30fe119f6bb843a58db7dc4849c8ea7f108
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 09:44:37 +0300

    Add example ++/--

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 
-console.log(12%0); //NaN  
 
-console.log(12*'qwerty'); //NaN
+let number1 = 1;
+let number2 = 2;
 
-console.log(12 + -true); //12
+console.log('prefix increment =', ++number1);
+console.log('number2 =', number2++);
+console.log('number2 =', number2);
 
-console.log(12 * undefined); //NaN
 
-console.log(12 - NaN); //NaN
-/*
-0 -0 +0
-''
-null
-false
-*/
-/*
-1
-true
-*/
-/*
-NaN
-0/0
-n%0
-/+-*% NaN || undefined
-*/
+
+
+
