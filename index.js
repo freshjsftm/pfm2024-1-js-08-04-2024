48ac1b48611e1558eb5b14e9ce3d062af28d2625
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 11:30:53 +0300

    Add example  equality

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 let number1 = 10;
-let number2 = 10;
+let number2 = '10';
 
-const resultLess = number1 < number2;
-console.log(resultLess);
-console.log(number1 >= number2);
-
-let str1 = 'cat';
-let str2 = 'category';
-let str3 = '102';
-let str4 = '2';
-
-console.log(str1 > str2);
-console.log(number1 > str3);
-console.log(number1 > true);
-console.log(number1 > undefined);
-console.log(number1 > null);
-console.log(number1 > NaN);
-console.log(str1 > NaN);
+console.log(number1 === number2); //false
+console.log(number1 !== number2); //true
