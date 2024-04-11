f8eab41eacb791aa3adc63df05c13b0b4c33b968
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-11 10:05:37 +0300

    Add example exponentiation

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
-let number1 = 1;
-let number2 = 2;
+const base1 = 2;
+const pow1 = 3;
+const result1 = base1 ** pow1;
+console.log('result1', result1);
 
-console.log('prefix increment =', ++number1);
-console.log('number2 =', number2++);
-console.log('number2 =', number2);
+const base2 = 2;
+const pow2 = -3;
+const result2 = base2 ** pow2;
+console.log('result2', result2);
 
+const base3 = 2;
+const pow3 = 3;
+const result3 = (-base3) ** pow3;
+console.log('result3', result3);
 
 
 
