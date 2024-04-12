38d543c01a63a51681a21b4fe28a18e2c1335aaa
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-12 10:37:45 +0300

    Add example use logical operators

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-//AND  &&  multiple
-/*
-true  && true  = true
-false && true  = false
-true  && false = false
-false && false = false
-*/
-//повертає останню правду або першу неправду
-const resultAND = NaN && null && 'qwe';
-console.log(resultAND);
-//OR  ||  add
-/*
-true  || true  = true
-false || true  = true
-true  || false = true
-false || false = false
-*/
-//повертає останню неправду або першу правду
-const resultOR = 0 || undefined || 2;
-console.log(resultOR);
-/*
-false  0   ''  NaN  undefined  null
-*/
+let number1 = 1;
+let number2 = 5;
+const elem = null;
+const condition = true;
+const str1 = ' '; //32
+const str2 = '!'; //33
+const str3 = ''; // '' !== ''
+// false 0 '' NaN null undefined
 
-//Nullish coalescing operator  ??
+//             false            true         !
+const check = str3 !== '' && condition && str2 ;
+console.log('check = ', check);//!
 
-//undefined  null
+//const check = !str3 && !elem && number1===number2;
+//1 !str3 = !'' = !false = true
+//2 !elem = !null = !false = true
+//3 number1===number2 = false
+//4 !false = true
 
-let termin;
-let data;
-console.log(10 ?? Infinity);
-console.log(termin ?? Infinity);
-console.log(data ?? 'данні за замовчуванням');
+//console.log('check = ', check);
+
+// const check = undefined ?? (number1 || elem) ;
+// number1 || elem  => 1 || null => 1
+// undefined ?? 1 => 1
+
+//const check = !elem || !condition && !number1;
+//1 !elem => !null => !false => true
+//2 !condition => !true => false
+//3 !number1 => !true => false
+//4 false && false => false
+//5 true || false => true
+//console.log('check = ', check);
+
+//const result = number1 < number2 && (elem || condition);
+//1 number1 < number2 = false
+//2 elem || condition = true
+//3 false && true = false
+
+const number3 = 6;
+// check diapason
+const result = number1 <= number3 && number3 <= number2;
+// 1 number1 < number = true
+// 2 number3 < number2 = false
+// 3 true && false = false
+
+//так не можна!!!
+//number1 < number3 < number2 => true<5 => 1<5 => true
+//console.log('result = ', result);
