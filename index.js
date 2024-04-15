82a8f36398be48f8e848872113e8bc8988af1de2
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-15 10:24:43 +0300

    Add getSumTwoNumbersFromUser version 1.0

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const userValue = prompt('enter number', 5);
+//написати функцію яка рахує суму двох чисел які ввів користувач
 
-console.log(userValue, typeof userValue);
+const getSumTwoNumbersFromUser = function () {
+  const userValue1 = prompt('enter number', 2);
+  const userValue2 = prompt('enter number', 3);
+
+  const checkUserInputError =
+    userValue1 === null ||
+    userValue2 === null ||
+    userValue1 === '' ||
+    userValue2 === '' ||
+    Number.isNaN(Number(userValue1)) ||
+    Number.isNaN(Number(userValue2));
+
+  if (checkUserInputError) {
+    console.log('error');
+  } else {
+    const summa = Number(userValue1) + Number(userValue2);
+    console.log(userValue1, ' + ', userValue2, ' = ', summa);
+  }
+};
+
+getSumTwoNumbersFromUser();
+
