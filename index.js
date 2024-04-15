4f997f713a6911ac9fe648fb099d275dd8618b85
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-15 09:44:06 +0300

    Add example use prompt

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-logMessage1();
-//declaration
-function logMessage1() {
-  console.log('hi, from function 1');
-}
-logMessage1();
+const userValue = prompt('enter number', 5);
 
-
-//expression
-const logMessage2 = function () {
-  console.log('hi, from function 2');
-};
-logMessage2();
-
-//arrow
-const logMessage3 =  () => {
-  console.log('hi, from function 3 arrow');
-};
-logMessage3();
+console.log(userValue, typeof userValue);
