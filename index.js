aa8926afa2ac07ce987e44444a78f2e234251b2d
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-15 09:26:28 +0300

    Add decloration variants

--- index.js
+++ index.js
@@ -1,76 +1,19 @@
-/*
-if (condition) {
-  statement1 
-} else {
-  statement2
+logMessage1();
+//declaration
+function logMessage1() {
+  console.log('hi, from function 1');
 }
-*/
-//......34567......
-
-let number1 = 15;
-const divisor = 5;
-const minRange = 11;
-const maxRange = 16;
-
-// !(3>number1  number1<7)&& !(number1%7===0)
-
-// написати вираз який буде правдивим якщо число НЕ кратне 5 і(або) НЕ в діапазоні [11-16) 
-//     false                         false              false
-if (number1%divisor !== 0 ||  number1<minRange || number1>=maxRange ) {
-  console.log('truthy');
-} else {
-  console.log('false');
-}
+logMessage1();
 
 
+//expression
+const logMessage2 = function () {
+  console.log('hi, from function 2');
+};
+logMessage2();
 
-// написати вираз який буде правдивим якщо число більше 0 і кратне 5 (ділится без остачі, тобто залишок === 0)
-
-// if (number1 > 0 && number1 % divisor === 0) {
-//   console.log('truthy');
-// } else {
-//   console.log('false');
-// }
-
-// написати вираз який буде правдивим якщо число НЕпарне
-
-if (number1 % 2 !== 0) {
-  console.log('odd');
-} else {
-  console.log('even');
-}
-
-if (number1 % 2) {
-  console.log('odd');
-} else {
-  console.log('even');
-}
-
-// написати вираз який буде правдивим якщо число парне
-
-// bad practice
-if (!(number1 % 2)) {
-  console.log('even');
-} else {
-  console.log('odd');
-}
-
-if (number1 % 2 === 0) {
-  console.log('even');
-} else {
-  console.log('odd');
-}
-
-// написати вираз який буде правдивим якщо число позитивне або дорівнює 0
-
-if (number1 >= 0) {
-  console.log('positive or zero');
-} else {
-  console.log('negative');
-}
-
-if (0 > number1) {
-  console.log('negative');
-} else {
-  console.log('positive or zero');
-}
+//arrow
+const logMessage3 =  () => {
+  console.log('hi, from function 3 arrow');
+};
+logMessage3();
