95da4d99e251cbd62bdffee9fadd801a463feebe
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-16 10:25:36 +0300

    Fix getMaxFromTwoNumbers

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // написати функцію, яка приймає два числа і повертає максимальне, якщо числа однакові - повертає будь яке
 
-console.log('написати функцію, яка приймає два числа і повертає максимальне, якщо числа однакові - повертає будь яке');
+console.log(
+  'написати функцію, яка приймає два числа і повертає максимальне, якщо числа однакові - повертає будь яке'
+);
 
 //const name = function (){}
 //function name (){}
 
 /**
  * функцію, яка приймає два числа і повертає максимальне, якщо числа однакові - повертає будь яке
- * @param {number} number1 
- * @param {number} number2 
+ * @param {number} number1
+ * @param {number} number2
  * @returns {number}
  */
 function getMaxFromTwoNumbers(number1, number2) {
+  if(Number.isNaN(number1) || Number.isNaN(number2)){
+    return NaN;
+  }
   if(number1 > number2){
     return number1;
   }
-  return number2; 
+  return number2;
 }
+
+// function getMaxFromTwoNumbers(number1, number2) {
+//   if (number1 >= number2) {
+//     return number1;
+//   }
+//   return number2;
+// }
