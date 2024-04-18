f54cc27a97bc27b9b94b4fdf9ae220dfb19af399
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-18 10:13:12 +0300

    Add ternary

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-let check;
-const value1 = 48;
-const value2 = 45;
-debugger;
-if (typeof value1 === typeof value2 && value1 > value2) {
-  check = value1;
-} else if (typeof value1 === typeof value2 && value2 > value1) {
-  check = value2;
-} else {
-  check = null;
-}
-////
+//тернарний
+// умова ? правдивий варіант : неправдивий варіант;
 
-if (typeof value1 === typeof value2) {
-  if (value1 > value2) {
-    check = value1;
-  } else if (value2 > value1) {
-    check = value2;
-  } else {
-    check = null;
-  }
-} else {
-  check = null;
+//написати функцію яка перевіряє на повноліття використовуючи тернарник
+//функція приймає вік людини(число) і повертає рядок!!! 'adult'  'not adult'
+
+/**
+ *
+ * @param {number} age
+ * @returns {string}
+ */
+function getStatusAdultUser(age) {
+  // if (age >= 18) {
+  //   return 'adult';
+  // }
+  // return 'not adult';
+  return age >= 18 ? 'adult' : 'not adult';
 }
+
