15cee970015cb01bd0c353a5e3ab00bcb7a5e56a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-19 11:43:40 +0300

    Add task about password

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
-// loop
+const VALID_PASSWORD = 'qwerty';
+const MAX_AMOUNT_TRY = 2;
 
-// передумова
-const MAX_VALUE = 3;
-//debugger
-for (let i = 0; i < MAX_VALUE; i++) {
-  console.log(i);
+// запитувати в користувача пароль до тих пір, доки не введе вірний
+while (true) {
+  const userInputPassword = prompt('Enter password');
+  if (userInputPassword === VALID_PASSWORD) {
+    console.log('welcome');
+    break;
+  }
 }
+//bad practice
+for (let i = 0; i < MAX_AMOUNT_TRY; i++) {
+  const userInputPassword = prompt('Enter password');
+  if (userInputPassword === VALID_PASSWORD) {
+    console.log('welcome');
+    break;
+  }
+}
+
+// запитувати в користувача пароль до тих пір, доки не введе вірний, але обмежити спроби кількістю 3 рази
+// let countTry = 0;
+// while (true) {
+//   const userInputPassword = prompt('Enter password');
+//   ++countTry;
+//   if(userInputPassword === VALID_PASSWORD) {
+//     console.log('welcome');
+//     break;
+//   }
+//   if(countTry >= MAX_AMOUNT_TRY){
+//     console.log('sorry');
+//     break;
+//   }
+// }
+
+// let countTry = 0;
+// while (countTry < MAX_AMOUNT_TRY) {
+//   const userInputPassword = prompt('Enter password');
+//   countTry++;
+//   if (userInputPassword === VALID_PASSWORD) {
+//     console.log('welcome');
+//     break;
+//   }
+//   if (countTry === MAX_AMOUNT_TRY) {
+//     console.log('sorry');
+//   }
+// }
+
+// for (let tryCount = 0; tryCount < MAX_AMOUNT_TRY; tryCount++) {
+//   const userInputPassword = prompt('Enter password');
+//   if (userInputPassword === VALID_PASSWORD) {
+//     console.log('welcome');
+//     break;
+//   }
+//   if (tryCount === MAX_AMOUNT_TRY - 1) {
+//     console.log('sorry');
+//   }
+// }
