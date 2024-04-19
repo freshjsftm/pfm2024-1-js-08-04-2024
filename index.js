3322b87e49bf39871c316470dd6cd0a9d34fb47f
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-19 10:38:42 +0300

    Add example loop for

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 // loop
-// передумова і післяумова
-let count = 10;
-const MAX_VALUE = 3;
+
 // передумова
-while (count < MAX_VALUE) {
-  count++;
-  console.log(count);
+const MAX_VALUE = 3;
+//debugger
+for (let i = 0; i < MAX_VALUE; i++) {
+  console.log(i);
 }
-//післяумова
-count = 10;
-do {
-  count++;
-  console.log(count);
-} while (count < MAX_VALUE);
-//післяумова
-count = 10;
-while (true) {
-  count++;
-  console.log(count);
-  if (count >= MAX_VALUE) break;
-}
-
-
