b2287f4b454e9ec06f153a44a1e090dc2998c489
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-19 12:27:06 +0300

    Add example continue

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
-const VALID_PASSWORD = 'qwerty';
-const MAX_AMOUNT_TRY = 2;
+// continue
 
-// запитувати в користувача пароль до тих пір, доки не введе вірний
-while (true) {
-  const userInputPassword = prompt('Enter password');
-  if (userInputPassword === VALID_PASSWORD) {
-    console.log('welcome');
-    break;
+const min = 10;
+const max = 30;
+const devisor = 7;
+
+// вивести числа з діапазона які кратні devisor
+
+//в зворотньому порядку
+for (let number = max; number >= min; number--) {
+  if (number % devisor) {
+    continue;
   }
-}
-//bad practice
-for (let i = 0; i < MAX_AMOUNT_TRY; i++) {
-  const userInputPassword = prompt('Enter password');
-  if (userInputPassword === VALID_PASSWORD) {
-    console.log('welcome');
-    break;
-  }
+  console.log(number);
 }
 
-// запитувати в користувача пароль до тих пір, доки не введе вірний, але обмежити спроби кількістю 3 рази
-// let countTry = 0;
-// while (true) {
-//   const userInputPassword = prompt('Enter password');
-//   ++countTry;
-//   if(userInputPassword === VALID_PASSWORD) {
-//     console.log('welcome');
-//     break;
-//   }
-//   if(countTry >= MAX_AMOUNT_TRY){
-//     console.log('sorry');
-//     break;
-//   }
-// }
+for (let number = min; number <= max; number++) {
+  if (number % devisor) {
+    continue;
+  }
+  console.log(number);
+}
 
-// let countTry = 0;
-// while (countTry < MAX_AMOUNT_TRY) {
-//   const userInputPassword = prompt('Enter password');
-//   countTry++;
-//   if (userInputPassword === VALID_PASSWORD) {
-//     console.log('welcome');
-//     break;
-//   }
-//   if (countTry === MAX_AMOUNT_TRY) {
-//     console.log('sorry');
-//   }
-// }
-
-// for (let tryCount = 0; tryCount < MAX_AMOUNT_TRY; tryCount++) {
-//   const userInputPassword = prompt('Enter password');
-//   if (userInputPassword === VALID_PASSWORD) {
-//     console.log('welcome');
-//     break;
-//   }
-//   if (tryCount === MAX_AMOUNT_TRY - 1) {
-//     console.log('sorry');
-//   }
-// }
+let number = min;
+while(number <= max){
+   if (number % devisor) {
+    number++;
+    continue;
+  }
+  console.log(number); 
+  number++;
+}
