fce60a8228a64978361f33eb915f5484a6aa77e2
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-19 10:20:29 +0300

    Add example loop while

--- index.js
+++ index.js
@@ -1,67 +1,24 @@
-//if  - складна умова
-//switch -вибір з певних варіантів
-
-//номер місяця - пору року
-//winter -1,2,12  spring-3,4,5   summer - 6, 7, 8   autumn - 9,10,11
-
-const monthUser = prompt('month'); // '' null  word
-const month = Number(monthUser); //  0  0     NaN
-//
-if (
-  month < 1 ||
-  month > 12 ||
-  Number.isNaN(month) ||
-  Number.isInteger(month) === false
-) {
-  console.log('wrong data');
-} else if (month <= 2 || month === 12) {
-  //123456789 10 11 12
-  console.log('winter');
-} else if (month <= 5) {
-  //3456789 10 11
-  console.log('spring');
-} else if (month <= 8) {
-  //6789 10 11
-  console.log('summer');
-} else {
-  //9 10 11
-  console.log('autumn');
+// loop
+// передумова і післяумова
+let count = 10;
+const MAX_VALUE = 3;
+// передумова
+while (count < MAX_VALUE) {
+  count++;
+  console.log(count);
+}
+//післяумова
+count = 10;
+do {
+  count++;
+  console.log(count);
+} while (count < MAX_VALUE);
+//післяумова
+count = 10;
+while (true) {
+  count++;
+  console.log(count);
+  if (count >= MAX_VALUE) break;
 }
 
-if (month === 1 || month === 2 || month === 12) {
-  console.log('winter');
-} else if (month === 3 || month === 4 || month === 5) {
-  console.log('spring');
-} else if (month === 6 || month === 7 || month === 8) {
-  console.log('summer');
-} else if (month === 9 || month === 10 || month === 11) {
-  console.log('autumn');
-} else {
-  console.log('error');
-}
 
-switch (month) {
-  case 1:
-  case 2:
-  case 12:
-    console.log('winter');
-    break;
-  case 3:
-  case 4:
-  case 5:
-    console.log('spring');
-    break;
-  case 6:
-  case 7:
-  case 8:
-    console.log('summer');
-    break;
-  case 9:
-  case 10:
-  case 11:
-    console.log('autumn');
-    break;
-  default:
-    console.log('error');
-    break;
-}
