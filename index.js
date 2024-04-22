7bd076a9b1aa16ec6748e5a4eaed76cb88d91010
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-22 10:37:07 +0300

    Add copy by refernces

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const object1 = new Object();
-object1.prop1 = 'prop1';
-const object2 = Object(7);
-
-const object = {}; //new Object()
-
-const button = {
-  text: 'click',
+const user = {
+  //222
+  fname: 'Brad',
+  sname: 'Pitt',
 };
 
-button.width = 50;
-button.bgcolor = 'blue';
+const activeUser = user; //222
+activeUser.age = 60;
 
-console.log(button);
+const userClient = activeUser; //222
+userClient.order = '123';
 
 
-const symbol = '*';
-//symbol = '-';
