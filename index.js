8df40480697d4a534342b3c46fba1bc113874ff8
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-23 11:32:04 +0300

    Add example  x in y,  for in

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 //ключем об'єкта може бути рядок або символ
-const nameProperty1 = 'fname';
-const nameProperty2 = 'sname';
+
 const user = {
   fname: 'Brad',
   sname: 'Pitt',
   age: 60,
   'two word': 'qwerty asdfgh',
   45: 333,
+  key: undefined,
+  show: function () {
+    return 'show';
+  },
 };
-console.log(user.fname);
-console.log(user[nameProperty1]);
-console.log(user['fname']);
-console.log(user[nameProperty2]);
-function getPropValue(nameProp, obj) {
-  return obj[nameProp];
-}
-console.log(getPropValue('age', user));
-console.log(getPropValue('fname', user));
-console.log(getPropValue(nameProperty2, user));
 
-console.log(user['two word']);
-console.log(user['age']);
-console.log(user[45]);
-console.log(user['45']);
+for (const key in user) {
+  console.log(key, ':', user[key]);
+}
+
+// key in obj
+console.log('fname' in user);
+console.log('fullname' in user);
+console.log('key' in user);
+console.log(45 in user);
+console.log('45' in user);
+console.log('show' in user);
+
+const prop = '45';
+if (prop in user) {
+  console.log(typeof user[prop] === 'function' ? user[prop]() : user[prop]);
+}
+
+
