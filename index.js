39772c22cbe5b22b44d94b895c4fe54b0d356ec2
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-23 12:21:25 +0300

    Add getMultTable

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 //ключем об'єкта може бути рядок або символ
+/*
+const multTable = {
+  '1 * 1 = ': 1,
+  '1 * 2 = ': 2,
+  '1 * 3 = ': 3,
+  '1 * 4 = ': 4,
+  '1 * 5 = ': 5,
+  '1 * 6 = ': 6,
+}
+*/
 
-const user = {
-  fname: 'Brad',
-  sname: 'Pitt',
-  age: 60,
-  'two word': 'qwerty asdfgh',
-  45: 333,
-  key: undefined,
-  show: function () {
-    return 'show';
-  },
-};
-
-for (const key in user) {
-  console.log(key, ':', user[key]);
+function getMultTable(firstNumber = 1) {
+  const multTable = {};
+  for (let i = 1; i <= 10; i++) {
+    multTable[`${firstNumber} * ${i} = `] = firstNumber * i;
+  }
+  return multTable;
 }
 
-// key in obj
-console.log('fname' in user);
-console.log('fullname' in user);
-console.log('key' in user);
-console.log(45 in user);
-console.log('45' in user);
-console.log('show' in user);
+console.log(getMultTable(5));
 
-const prop = '45';
-if (prop in user) {
-  console.log(typeof user[prop] === 'function' ? user[prop]() : user[prop]);
+const userTask = '5 * 3 = ';
+const multTable = getMultTable(5);
+
+console.log(multTable[userTask]);
+
+function getMultTableMany(min = 1, max = 9) {
+  const multTable = {};  //address references
+  for (let firstNumber = min; firstNumber <= max; firstNumber++) {
+    for (let i = 1; i <= 10; i++) {
+      multTable[`${firstNumber} * ${i} = `] = firstNumber * i;
+    }
+  }
+  return multTable;  //address references
 }
 
-
+console.log(getMultTableMany());
