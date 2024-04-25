82f43dbc5baf8045f7fe6b7eef0ab9407f7196d1
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-25 11:17:06 +0300

    Add example constructor for protype

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ console.log(arrNums);
 const arrNums2 = [];
 console.log(arrNums.push === arrNums2.push);
 
-const myArrayPrototype = {};
-myArrayPrototype.push = function (value) {
-  this[this.length] = value; 
-  this.length++;
-  return this.length;
-};
+// const myArrayPrototype = {
+//   push: function (value) {
+//     this[this.length] = value;
+//     this.length++;
+//     return this.length;
+//   },
+// };
 
+//конструктор для прототипу
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  };
+}
+//конструктор для данних (для сутності інстанса)
 function MyArray() {
   this.length = 0;
 }
-
-MyArray.prototype = myArrayPrototype;
-
+// зв'язок між конструктором для данних та об'єктом проптотипу
+MyArray.prototype = new MyArrayPrototype();
 
 const myArrNums = new MyArray();
 myArrNums.push(20);
