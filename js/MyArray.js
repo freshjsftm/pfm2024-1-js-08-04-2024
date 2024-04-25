76507c1f0a718dc3b5ca5941263df8164aae1d0d
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-25 11:26:52 +0300

    Create MyArray.js

---
+++ js/MyArray.js
@@ -0,0 +1,14 @@
+//конструктор для прототипу
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  };
+}
+//конструктор для данних (для сутності інстанса)
+function MyArray() {
+  this.length = 0;
+}
+// зв'язок між конструктором для данних та об'єктом проптотипу
+MyArray.prototype = new MyArrayPrototype();
