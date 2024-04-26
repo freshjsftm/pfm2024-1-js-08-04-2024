f059fe5d10471b7d5f7661a254a53ab60985f3d1
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-26 12:09:04 +0300

    Add example slice

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const arrNums1 = [4, 7, 5, 6, 7, 8, 9, 7];
+const arrNums1 = [4, 7, 5, 6, 8, 9];
 console.log(arrNums1);
 
-const elem = 7;
-const fromIndex = 2;
-const indexFirstElem = arrNums1.indexOf(elem, fromIndex);
-const indexLastElem = arrNums1.lastIndexOf(elem, fromIndex);
+// const newArrNums1 = arrNums1;//copy address
+// newArrNums1.splice(0,1,77);
+// newArrNums1[0] = 88;
 
-console.log(indexFirstElem);
-console.log(indexLastElem);
+const arrNums1Copy = arrNums1.slice(); //copy all array
+arrNums1Copy[0] = 2222;
+console.log(arrNums1Copy.at(0))
+console.log(arrNums1Copy);
+// console.log(arrNums1);
 
-// const colors = ['green', 'red', 'white', 'pink', 'red', 'aqua'];
-// const key = 'red';
-// console.log(colors);
-// console.log(colors.indexOf(key)); //1
-// console.log(colors.lastIndexOf(key)); //4
+const start = 1;
+const end = 4;
 
-// console.log(colors.indexOf('blue'));
-// console.log(colors.lastIndexOf('blue'));
+const arrNums1CopyPart = arrNums1.slice(start);
+console.log(arrNums1CopyPart);
+console.log(arrNums1);
 
-// console.log(colors.indexOf('white'));
-// console.log(colors.lastIndexOf('white'));
+const arrNums1CopyPart2 = arrNums1.slice(start, end);
+console.log(arrNums1CopyPart2);
+console.log(arrNums1);
