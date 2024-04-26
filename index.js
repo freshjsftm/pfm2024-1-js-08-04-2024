67a437e8299683b037112d51238412ae4f570464
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-26 09:44:01 +0300

    Add example pop

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-const userFunc = {
-  getFullName: function () {
-    return `${this.fname} ${this.sname}`;
-  },
-  getInfo: function () {
-    return `${this.sname}, years old ${this.age}`;
-  },
-};
-
-const user1 = {
-  fname: 'Brad',
-  sname: 'Pitt',
-  age: 60,
-};
-user1.__proto__ = userFunc;
-
-const user2 = {
-  fname: 'Tom',
-  sname: 'Rot',
-  age: 59,
-  __proto__: userFunc,
-};
+console.log([]);
 
 
-function User(fname, sname, age) {
-  this.fname = fname;
-  this.sname = sname;
-  this.age = age;
-}
+const arrNums1 = [4,5,6];
+console.log(arrNums1[arrNums1.length-1]);
 
-function UserPrototype() {
-  this.sayHello = function (){
-    return `hello , I'm  ${this.fname}`;
-  }
-  this.getFullName = function () {
-    return `${this.fname} ${this.sname}`;
-  };
-  this.getInfo = function () {
-    return `${this.sname}, years old ${this.age}`;
-  };
-}
+console.log(arrNums1);
+console.log(arrNums1.pop());
+console.log(arrNums1.pop());
+console.log(arrNums1.pop());
+console.log(arrNums1.pop());
+console.log(arrNums1.pop());
+console.log(arrNums1.pop());
+console.log(arrNums1);
 
-User.prototype = new UserPrototype();
-
-const user3 = new User('Anna', 'Zeferino', 23);
-const user4 = new User('Maria', 'Fox', 23);
+const arrNums2 = arrNums1; //copy address
--- js/MyArray.js
+++ js/MyArray.js
@@ -5,7 +5,15 @@ function MyArrayPrototype() {
     this.length++;
     return this.length;
   };
-  this.pop = function () {};
+  this.pop = function () {
+    if(this.length===0){
+      return;
+    }
+    const lastItem = this[this.length-1];
+    delete this[this.length-1];
+    this.length--;
+    return lastItem;
+  };
   this.forEach = function () {};
 }
 //конструктор для данних (для сутності інстанса)
@@ -15,5 +23,13 @@ function MyArray() {
 // зв'язок між конструктором для данних та об'єктом проптотипу
 MyArray.prototype = new MyArrayPrototype();
 
-
-
+const myArrNums1 = new MyArray();
+myArrNums1.push(9);
+// myArrNums1.push(8);
+// myArrNums1.push(7);
+console.log(myArrNums1);
+console.log(myArrNums1.pop()); 
+myArrNums1.pop();
+myArrNums1.pop();
+myArrNums1.pop();
+console.log(myArrNums1);
