709abd6e8ccf7d22448603155d1a513dc55e8a47
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-26 11:46:00 +0300

    Add example indexOf & lastIndexOf

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
-// const arrNums1 = [4, 5, 6, 7, 8, 9];
-// console.log('arrNums1', arrNums1);
-// const startIndex = -3;
-// const deleteCount = 2;
-// const newItem1 = 100;
-// const newItem2 = 101;
-// const newItem3 = 102;
-// const arrNums1ReturnSplice = arrNums1.splice(
-//   startIndex,
-//   deleteCount,
-//   newItem1,
-//   newItem2,
-//   newItem3
-// );
-// console.log('return splice', arrNums1ReturnSplice);
+const arrNums1 = [4, 7, 5, 6, 7, 8, 9, 7];
+console.log(arrNums1);
 
-// console.log('arrNums1', arrNums1);
+const elem = 7;
+const fromIndex = 2;
+const indexFirstElem = arrNums1.indexOf(elem, fromIndex);
+const indexLastElem = arrNums1.lastIndexOf(elem, fromIndex);
 
-// const dayWeeks = ['sun', 'mon', 'tue', 'wen', 'thu', 'fri', 'sat'];
-// //видалити елемент з індексом 2
-// console.log(dayWeeks.splice(2, 1));
+console.log(indexFirstElem);
+console.log(indexLastElem);
 
-const colors = ['red', 'white', 'pink', 'aqua'];
+// const colors = ['green', 'red', 'white', 'pink', 'red', 'aqua'];
+// const key = 'red';
+// console.log(colors);
+// console.log(colors.indexOf(key)); //1
+// console.log(colors.lastIndexOf(key)); //4
 
-//splice
-const startIndex = Math.floor(colors.length / 2);
-const deleteCount = 0;
-const newItem1 = 'green';
-//додати в середину елемент 'green'
-console.log(colors.splice(startIndex, deleteCount, newItem1));
-console.log(colors);
-//замінити 'aqua' на 'blue'
-console.log(colors.splice(-1, 1, 'blue'));
-console.log(colors);
-//після 'red' вставити 'mangenta' та 'fucsia'
-console.log(colors.splice(1, 0, 'mangenta', 'fucsia'));
-console.log(colors);
-//видалити усі елементи починаючи з третього з кінця
-console.log(colors.splice(-3));
-console.log(colors);
+// console.log(colors.indexOf('blue'));
+// console.log(colors.lastIndexOf('blue'));
+
+// console.log(colors.indexOf('white'));
+// console.log(colors.lastIndexOf('white'));
