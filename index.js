2cce89ad2d6d038c6cc6f4624fc8f41e64a49777
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-29 12:15:46 +0300

    Add example sort & toSorted

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
-const arrNums1 = [1, 5, 2, 4, 6];
+const arrNums1 = [1, 5, 12, 4, 6, 101, 111];
+
+//arrNums1.sort(compareNumbers);
 console.log(arrNums1);
-//map
-function incrementElem(elem) {
-  return ++elem;
+console.log(arrNums1.toSorted(compareNumbers));
+console.log(arrNums1);
+
+function compareNumbers(a, b) {
+  return a - b;
 }
-const newArrNums1 = arrNums1.map(incrementElem);
-console.log(newArrNums1);
 
 const users = [
-  { id: 100, login: 'qwe1', isMale: true },
-  { id: 200, login: 'qwe21', isMale: true },
-  { id: 101, login: 'asd', isMale: false },
+  { id: 101, login: 'qwe1', isMale: true },
+  { id: 102, login: 'qwe21', isMale: true },
+  { id: 141, login: 'asd', isMale: false },
   { id: 178, login: 'sss', isMale: false },
-  { id: 10, login: 'max', isMale: true },
+  { id: 100, login: 'max', isMale: true },
 ];
-//зібрати в масив лише логіни
-const logins = users.map(function (user) {
-  return user.login;
-});
-console.log(logins);
-//зібрати в масив лише id
-const ids = users.map(function (user) {
-  return user.id;
-});
-console.log(ids);
 console.table(users);
-//зібрати в масив лише логіни чоловіків
-const men = users.filter(function (user) {
-  return user.isMale;
-});
-const menLogins = men.map(function (user) {
-  return user.login;
-});
-console.log(menLogins);
 
-const menLogins2 = users
-  .filter(function (user) {
-    return user.isMale;
-  })
-  .map(function (user) {
-    return user.login;
-  });
-console.log(menLogins2);
+users.sort(function(item1, item2){
+  return item1.id - item2.id;
+})
+console.table(users);
+
+users.sort(function(user1, user2){
+  if(user1.login < user2.login)return -1;
+  if(user1.login > user2.login)return 1;
+  return 0;
+})
+console.table(users);
+
 
 const products = [
   { name: 'Молоко', price: 25, currency: 'грн', quantity: 29 },
@@ -51,13 +39,9 @@ const products = [
   { name: 'Сік', price: 33, currency: 'грн', quantity: 32 },
   { name: 'Печиво', price: 7, currency: 'грн', quantity: 120 },
 ];
+console.table(products);
 
-//відібрати назви всіх продуктів з ціною менше 20 грн
-const productsName = products
-  .filter(function (product) {
-    return product.price < 20;
-  })
-  .map(function (product) {
-    return product.name;
-  });
-console.log(productsName);
+products.sort(function(item1, item2){
+  return item1.price - item2.price;
+})
+console.table(products);
