3c7f6ab47770077617e2c8be2fb10f859baf2b9e
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-30 12:48:49 +0300

    Add concat & spred

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
 'use strict';
-const sumNumbers1 = function () {
-  // const arrArgs = Array.from(arguments);
-  return Array.from(arguments).reduce((summa, elem) => summa + elem);
-};
-const result = sumNumbers1(1, 2, 3);
-//console.log(result);
-
-const sumNumbers12 = function (num1, ...rest) {
-  console.log(arguments);
-  console.log(num1);
-  console.log(rest);
-  return rest.reduce((summa, elem) => summa + elem, num1);
-};
-const result12 = sumNumbers12(1, 2, 3, 4, 5, 6);
-console.log(result12);
-
-//rest
-const sumNumbers2 = (num1, ...rest) => {
-  //console.log(arguments); // - deprecated
-  // console.log(num1);
-  // console.log(rest);
-  return rest.reduce((summa, elem) => summa + elem, num1);
-};
-const result2 = sumNumbers2(1, 2, 3);
-console.log(result2);
 
 //spred
-const arrNums = [1,1,1];
-console.log(arrNums);
-console.log(arrNums[0], arrNums[1], arrNums[2]);
-console.log(...arrNums);
-console.log(Math.max(7,3,45))
-console.log(Math.max(...arrNums))
-console.log(sumNumbers2(...arrNums));
+const arrNums1 = [1,1,1];
+const arrNums2 = [2,2,2];
+const arrNums3 = [3,3,3];
 
+const arrNumsConcat = arrNums1.concat(arrNums2, arrNums3);
 
+console.log(arrNumsConcat);
+console.log(arrNums1);
+console.log(arrNums2);
 
+const arrNumsSpred = [...arrNums3, ...arrNums1, ...arrNums2];
+console.log(arrNumsSpred);
+
+const copyArrNums1 = [...arrNums1]; // [1,1,1]
+console.log(copyArrNums1);
+console.log(copyArrNums1===arrNums1);
