f7391da6f49510df1fc5890cbdd22f2894190821
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-30 10:52:15 +0300

    Add example use arrow context

--- index.js
+++ index.js
@@ -1,41 +1,23 @@
 'use strict';
 // debugger
-
-const logHead3 = function (head, index) {
-  console.log(index + 1, head, '|', this.title);
-}
-
-const site = {
-  title: 'my site',
-  headings: ['About us', 'Contact', 'News'],
-  // 1 збереження контекста - милиці
-  logHeadings1: function () {
-    const self = this;
-    this.headings.forEach(function (head) {
-      console.log(head, '|', self.title);
-    });
-  },
-  // 2 передати контекст методу перебору масиву
-  logHeadings2: function () {
-    this.headings.forEach(function (head, index) {
-      console.log(index + 1, head, '|', this.title);
-    }, this);
-  },
-  // 3 прив'язувати контекст за допомогою методa функцій bind
-  logHeadings3: function () {
-    this.headings.forEach(logHead3.bind(this));
-  },
-  // 4 arrow
-  logHeadings4: function () {
-    this.headings.forEach((head) => {
-      console.log(head, '|', this.title);
-    });
-  },
+const dataProto = {
+  addFrom: function(){
+    this.users.forEach((user)=>{
+      user.from = this.from;
+    })
+  }
 };
-
-site.logHeadings1();
-site.logHeadings2();
-site.logHeadings3();
-site.logHeadings4();
-
-
+const data = {
+  error: null,
+  from: 'db',
+  users: [
+    { id: 100, login: 'qwe1', isMale: true },
+    { id: 200, login: 'qwe21', isMale: true },
+    { id: 101, login: 'asd', isMale: false },
+    { id: 178, login: 'sss', isMale: false },
+    { id: 10, login: 'max', isMale: true },
+  ],
+  __proto__: dataProto,
+};
+data.addFrom();
+console.table(data.users)
