e3dc24ce16c194854fb7d657832a65e4a042ec0e
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-04-30 10:39:11 +0300

    Add 4 variant adding context

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-'use strict'; 
+'use strict';
 // debugger
 
-//declaration
-function test1(){
-  console.log('test1', this)
+const logHead3 = function (head, index) {
+  console.log(index + 1, head, '|', this.title);
 }
-test1()
-//expression
-const test2 = function(){
-  console.log('test2', this)
-}
-test2()
 
-//arrow
-const test3 = ()=>{
-  console.log('test3', this)
-}
-test3()
+const site = {
+  title: 'my site',
+  headings: ['About us', 'Contact', 'News'],
+  // 1 збереження контекста - милиці
+  logHeadings1: function () {
+    const self = this;
+    this.headings.forEach(function (head) {
+      console.log(head, '|', self.title);
+    });
+  },
+  // 2 передати контекст методу перебору масиву
+  logHeadings2: function () {
+    this.headings.forEach(function (head, index) {
+      console.log(index + 1, head, '|', this.title);
+    }, this);
+  },
+  // 3 прив'язувати контекст за допомогою методa функцій bind
+  logHeadings3: function () {
+    this.headings.forEach(logHead3.bind(this));
+  },
+  // 4 arrow
+  logHeadings4: function () {
+    this.headings.forEach((head) => {
+      console.log(head, '|', this.title);
+    });
+  },
+};
+
+site.logHeadings1();
+site.logHeadings2();
+site.logHeadings3();
+site.logHeadings4();
+
+
