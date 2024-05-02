4cbbd0219686ace727319071f0dcb715e61411d4
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-02 10:42:37 +0300

    Add recursia & throw

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
 'use strict';
+// recursia
+// не використовувати рекурсію
+// описати базовий випадок
 
-//spred
-const arrNums1 = [1,1,1];
-const arrNums2 = [2,2,2];
-const arrNums3 = [3,3,3];
+function recursia(number = 5) {
+  console.log(number);
+  if (number === 1) {
+    return;
+  }
+  return recursia(number - 1);
+}
+// debugger
+// recursia();
 
-const arrNumsConcat = arrNums1.concat(arrNums2, arrNums3);
+console.log('code ');
 
-console.log(arrNumsConcat);
-console.log(arrNums1);
-console.log(arrNums2);
+// (2 ** 5) = 2 * (2 ** 4)  
+// (2 ** 4) = 2 * (2 ** 3) 
+// (2 ** 3) = 2 * (2 ** 2) 
+// (2 ** 2) = 2 * (2 ** 1) 
+// (2 ** 1) = 2 * (2 ** 0) 
+// (2 ** 0) = 1 
 
-const arrNumsSpred = [...arrNums3, ...arrNums1, ...arrNums2];
-console.log(arrNumsSpred);
+function powRecursion (number = 2, exp = 2){
+  if(exp === 0){
+    return 1;
+  }
+  return number * powRecursion(number, exp-1);
+}
+console.log(powRecursion(2,6));
 
-const copyArrNums1 = [...arrNums1]; // [1,1,1]
-console.log(copyArrNums1);
-console.log(copyArrNums1===arrNums1);
+// 4! = 4 * 3!
+// 3! = 3 * 2!
+// 2! = 2 * 1!
+// 1! = 1
+
+/**
+ * 
+ * @param {number} number 
+ * @returns {number}
+ */
+function factorialRecursion(number = 0){
+  if(typeof number !== 'number'){
+    throw new TypeError('type must be number');
+  }
+  if(number>18 || number<0){
+    throw new RangeError('number must be positive <=18');
+  }
+  if(number === 0 || number === 1){
+    return 1;
+  }
+  return number * factorialRecursion(number-1);
+}
+console.log(factorialRecursion(-12));
+/**
+ * 
+ * @param {bigint} number 
+ * @returns {bigint}
+ */
+function factorialRecursionBigInt(number = 0n){
+  if(number<0n){
+    throw new RangeError('number must be positive');
+  }
+  if(typeof number !== 'bigint'){
+    throw new TypeError('type must be number');
+  }
+  if(number === 0n || number === 1n){
+    return 1n;
+  }
+  return number * factorialRecursionBigInt(number-1n);
+}
+console.log(factorialRecursionBigInt(-24n));
+
+
+console.log('footer');
