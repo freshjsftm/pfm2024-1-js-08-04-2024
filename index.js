9c40d825045f263a43281e4b97e6f2bbe0679a33
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-02 12:06:06 +0300

    Add new Bollean

--- index.js
+++ index.js
@@ -1,84 +1,27 @@
 'use strict';
 
-function recursia(number = 5) {
-  console.log(number);
-  if (number === 1) {
-    return;
-  }
-  return recursia(number - 1);
+let booleanValue = false;
+
+const booleanObj = new Boolean(false); //address
+console.log(booleanObj);
+// console.log(typeof booleanObj.toString(), booleanObj.toString());
+// console.log(typeof booleanObj.valueOf(), booleanObj.valueOf());
+
+if (booleanValue) {
+  console.log('booleanValue = ', booleanValue);
 }
 
-try {
-  //recursia(12000);
-} catch (error) {
-  console.log(error);
+if (booleanObj) {
+  console.log('booleanObj value = ', booleanObj.valueOf());
 }
 
-console.log('code ');
+const test = true.toString();
 
-function powRecursion(number = 2, exp = 2) {
-  if (exp === 0) {
-    return 1;
-  }
-  return number * powRecursion(number, exp - 1);
-}
-console.log(powRecursion(2, 6));
+const test2 = new Boolean(true).toString();
 
-/**
- *
- * @param {number} number
- * @returns {number}
- * @throws {TypeError} type must be number
- * @throws {Error} type must be integer
- * @throws {RangeError} number must be positive <=18
- */
-function factorialRecursion(number = 0) {
-  if (typeof number !== 'number') {
-    throw new TypeError('type must be number');
-  }
-  if (Number.isInteger(number) === false) {
-    throw new Error('type must be integer');
-  }
-  if (number > 18 || number < 0) {
-    throw new RangeError('number must be positive <=18' + number);
-  }
-  if (number === 0 || number === 1) {
-    return 1;
-  }
-  return number * factorialRecursion(number - 1);
-}
+const test3 = Boolean('');
+console.log(test3);
 
-try {
-  console.log(factorialRecursion(1.3));
-  console.log('info under factorialRecursion');
-} catch (error) {
-  console.error('error ----> ', error);
-} finally {
-  console.info('finally after factorialRecursion');
-}
 
-/**
- *
- * @param {bigint} number
- * @returns {bigint}
- */
-function factorialRecursionBigInt(number = 0n) {
-  if (number < 0n) {
-    throw new RangeError('number must be positive');
-  }
-  if (typeof number !== 'bigint') {
-    throw new TypeError('type must be number');
-  }
-  if (number === 0n || number === 1n) {
-    return 1n;
-  }
-  return number * factorialRecursionBigInt(number - 1n);
-}
-
-try {
-  console.log(factorialRecursionBigInt(24n));
-} catch (error) {
-  console.log(error);
-}
-
-console.log('footer');
+const value1 = 5;
+const value2 = true;
