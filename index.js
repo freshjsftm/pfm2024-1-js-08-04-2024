5535454426358c38f709bcc1982fa9a62cb7946a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-03 11:05:51 +0300

    Add sumDigitsOfNumber

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
 'use strict';
 
-const strObj = new String('word');
-console.log(strObj);
-console.log(strObj.valueOf());
+const number = 12321;
 
-console.log(String.fromCharCode(195));
-console.log(String.fromCodePoint(9777));
+// порахувати суму цифр числа
+// 1+2+3+2+1 = 9
 
-let str1 = 'Cat' + '!C';
-console.log(str1.length);
-console.log(typeof str1);
+//привести число до рядка!
+const numberStr = number.toString();
+const numberStr2 = String(number);
+//split - розбиває рядок на елементи масива
+const arrNumSymb = numberStr.split('');
+console.log(arrNumSymb);
+//reduce - порахувати суму елементів масива
+const summa = arrNumSymb.reduce((sum, item) => sum + Number(item), 0);
+console.log(summa);
 
-//toUpperCase toLowerCase
-console.log(str1.toUpperCase());
-console.log(str1.toLowerCase());
+const sumDigitsOfNumber = (number=111) => {
+  const numberStr = number.toString();
+  const arrNumSymb = numberStr.split('');
+  const summa = arrNumSymb.reduce((sum, item) => sum + Number(item), 0);
+  return summa;
+}
 
-//str1[0] = 'R'; //неможливо!!!!
+const sumDigitsOfNumber2 = (number = 0) =>
+  number
+    .toString()
+    .split('')
+    .reduce((sum, item) => sum + Number(item), 0);
 
-console.log(str1[0]); //imperative bad practice
-console.log(str1.at(-1)); //declarative good practice
-console.log(str1.charAt(1)); //declarative good practice
-console.log(str1.charCodeAt(1));
-
-console.log(str1.padStart(25, '.'));
-console.log(str1.repeat(7));
-console.log(str1.replace('C', 'R'));
-
-//str1 = str1.replaceAll('C','R');
-console.log(str1);
-console.log(str1.split('!'));
-console.log(str1.split(''));
-
-console.log(str1.slice(3, 1));
-console.log(str1.substring(3, 1));
-console.log(str1.substring(1, 3));
-
-let str2 = '      q       we        ';
-console.log(str2);
-console.log(str2.trim());
-console.log(str2);
+console.log(sumDigitsOfNumber(1010101));
+console.log(sumDigitsOfNumber2(10101));
