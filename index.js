6dea0c03ee843dda7b3677b73e0fe7d4f2d64aac
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-03 12:05:07 +0300

    Add toJadenCase

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 'use strict';
 
-const dataBDUser = '12/03/2000';
+const str = 'tO    bE    OR    nOt     To Be';
+console.log(str);
+// Jaden Case
+// 'To Be Or Not To Be'
 
-const arrDataBDUser = dataBDUser.split('/');
+// привести весь рядок до нижнього регистру toLowerCase
+// 'to be or not to be'
+const strLowerCase = str.toLowerCase();
+console.log(strLowerCase);
+// отримати масив слів split
+// ['to', 'be', 'or', 'not', 'to', 'be']
+const words = strLowerCase.split(' ')
+console.log(words);
+//прибираємо усі порожні елементи
+const wordsWithoutEmpty =  words.filter((word) => word);
+// з кожного елементу масива отримати новий елемент    map
+// де перша літера - велика, а всі інші - маленькі
+// ['To', 'Be', 'Or', 'Not', 'To', 'Be']
+const newWords = wordsWithoutEmpty.map((word) => {
+  const firstLetterUpper = word.at(0).toUpperCase();
+  const otherLetters = word.substring(1);
+  const newWord = firstLetterUpper + otherLetters;
+  return newWord;
+});
+console.log(newWords);
+// зібрати масив в рядок   join
+const strJadenCase = newWords.join(' ');
+console.log(strJadenCase);
 
-const yearBDUser = arrDataBDUser.at(-1);
-console.log(yearBDUser);
+// const toJadenCase = (str) => {
+//   const strLowerCase = str.toLowerCase();
+//   const words = strLowerCase.split(' ');
+//   const wordsWithoutEmpty =  words.filter((word) => word);
+//   const newWords = wordsWithoutEmpty.map((word) => {
+//     const firstLetterUpper = word.at(0).toUpperCase();
+//     const otherLetters = word.substring(1);
+//     const newWord = firstLetterUpper + otherLetters;
+//     return newWord;
+//   });
+//   const strJadenCase = newWords.join(' ');
+//   return strJadenCase;
+// };
 
-//Date
-//мітка Юнікс = 1970-01-01
+const toJadenCase = (str) =>
+  str
+    .split(' ')
+    .filter((word) => word)
+    .map((word) => word.at(0).toUpperCase() + word.substring(1).toLowerCase())
+    .join(' ');
 
-const now = new Date();
-const currentYear = now.getFullYear();
-console.log(currentYear);
-console.dir(now);
-
-//порахувати вік
-const age = currentYear - Number(yearBDUser);
-console.dir(age);
+console.log(toJadenCase(str));
