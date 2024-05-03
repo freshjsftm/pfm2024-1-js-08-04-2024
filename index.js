3627abb5d2f66c258cca7734640bc567f56c322a
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-03 11:32:22 +0300

    Add task2 with Date

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 'use strict';
 
-const number = 12321;
+const dataBDUser = '12/03/2000';
 
-// порахувати суму цифр числа
-// 1+2+3+2+1 = 9
+const arrDataBDUser = dataBDUser.split('/');
 
-//привести число до рядка!
-const numberStr = number.toString();
-const numberStr2 = String(number);
-//split - розбиває рядок на елементи масива
-const arrNumSymb = numberStr.split('');
-console.log(arrNumSymb);
-//reduce - порахувати суму елементів масива
-const summa = arrNumSymb.reduce((sum, item) => sum + Number(item), 0);
-console.log(summa);
+const yearBDUser = arrDataBDUser.at(-1);
+console.log(yearBDUser);
 
-const sumDigitsOfNumber = (number = 111) => {
-  const numberStr = number.toString();
-  const arrNumSymb = numberStr.split('');
-  const summa = arrNumSymb.reduce((sum, item) => sum + Number(item), 0);
-  return summa;
-};
+//Date
+//мітка Юнікс = 1970-01-01
 
-const sumDigitsOfNumber2 = (number = 0) =>
-  number
-    .toString()
-    .split('')
-    .reduce((sum, item) => sum + Number(item), 0);
+const now = new Date();
+const currentYear = now.getFullYear();
+console.log(currentYear);
+console.dir(now);
 
-const sumDigitsOfNumber3 = function (number = 0) {
-  return number
-    .toString()
-    .split('')
-    .reduce((sum, item) => sum + Number(item), 0);
-};
-
-console.log(sumDigitsOfNumber(1010101));
-console.log(sumDigitsOfNumber2(10101));
-console.log(sumDigitsOfNumber3(10101));
+//порахувати вік
+const age = currentYear - Number(yearBDUser);
+console.dir(age);
