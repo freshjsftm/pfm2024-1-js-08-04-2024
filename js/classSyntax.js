e6cbc6ed27415e3329521f636c41334f8ee3d30b
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-06 11:31:25 +0300

    Add example accessors with private fields

--- js/classSyntax.js
+++ js/classSyntax.js
@@ -11,34 +11,60 @@ class User {
    * @param {number} age
    */
   constructor(fname, lname, age) {
-    if (typeof fname !== 'string') {
-      throw new TypeError('first name must be string');
-    }
-    if (typeof lname !== 'string') {
-      throw new TypeError('last name must be string');
-    }
-    if (typeof age !== 'number') {
-      throw new TypeError('age must be number');
-    }
-    this.#fname = fname;
-    this.#lname = lname;
-    this.#age = age;
+    this.fname = fname; // fname(fname)
+    this.lname = lname;
+    this.age = age;
+    this.isSubscribe = false;
   }
   getFullName() {
     return `${this.#fname} ${this.#lname}`;
   }
-  getAge() {
+  //accessors - getter & setter
+  get fname() {
+    return this.#fname;
+  }
+  set fname(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('value must be string');
+    }
+    if (value.trim().length < 3) {
+      throw new RangeError('length must be >= 3');
+    }
+    this.#fname = value;
+  }
+  get lname() {
+    return this.#fname;
+  }
+  set lname(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('value must be string');
+    }
+    if (value.trim().length < 2) {
+      throw new RangeError('length must be >= 2');
+    }
+    this.#lname = value;
+  }
+  get age() {
     return this.#age;
   }
-  setAge(value){
+  set age(value) {
     if (typeof value !== 'number') {
       throw new TypeError('value must be number');
+    }
+    if (value <= 0 || value > 150) {
+      throw new RangeError('must be in diapazon 1-150');
     }
     this.#age = value;
   }
 }
-const userClasses = new User('Brad', 'Pitt', 60);
-userClasses.setAge(20);
-// userClasses.#age = 50; //Error
-//console.log(userClasses.#fname);  //Error
-console.log(userClasses);
+
+try {
+  const userClasses = new User('Brad', 'Pitt', 60);
+  console.log(userClasses.fname);
+  userClasses.age = 45;
+  console.log(userClasses.age);
+} catch (error) {
+  console.log(error);
+}
+
+console.log('code');
--- js/protoSyntax.js
+++ js/protoSyntax.js
@@ -14,13 +14,10 @@ function User1(fname, lname, age) {
   this.lname = lname;
   this.age = age;
 }
-
 function UserProto() {
   this.getFullName = function () {
     return `${this.fname} ${this.lname}`;
   };
 }
-
 User1.prototype = new UserProto();
-
 const user1 = new User1('Tom', 'Rot', 59);
