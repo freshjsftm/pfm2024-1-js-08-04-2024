1c02b5722e156a14b4a870863f61c60729283d09
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-09 11:17:13 +0300

    Add example create obj without new

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 'use strict';
 
+function MyObj(prop) {
+  if (!new.target) {
+    return new MyObj(prop);
+  }
+  this.prop = prop;
+}
+
+const obj1 = MyObj(78);
+console.log(obj1);
+
+const obj = Object();
