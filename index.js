cab6dfa204724fdbc7554cd244ccf945780633b3
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-16 13:36:39 +0300

    Add destructurisation

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
 'use strict';
 
-//Set
-const sum = (a, b) => a + b;
-const arr1 = [4, 5, 9, 4, 8, 6, 9, 4, 9];
-console.log(arr1);
-const setArr1 = new Set(arr1); //{4, 5, 9, 8, 6}
-console.log(setArr1);
-const uniqArr1 = [...setArr1]; //[4, 5, 9, 8, 6]
-console.log(uniqArr1);
-//short syntax
-const uniqArr2 = [...new Set(arr1)];
-console.log(uniqArr2);
+//деструктуризація - спосіб створення змінної
 
-const set1 = new Set();
-console.log(set1);
-set1.add(1);
-set1.add(1);
-set1.add('1');
-set1.add(sum);
-set1.add(sum(4, 8));
-set1.add(sum());
-set1.add(sum());
-set1.add(arr1);
-set1.add();
-set1.add(sum);
-set1.delete(1);
+const user = {
+  public1: 44,
+  privateInfo: {
+    id: 15,
+    login: 'Brad',
+    email: 'brad@gmail.com',
+    bd: {
+      year: 1963,
+      month: 12,
+      day: 18,
+    },
+  },
+  contactInfo: {
+    address: {
+      town: 'NY',
+      street: '15 avenu',
+      house: 547,
+    },
+    phone: '123-56-85',
+  },
+  generalInfo: {
+    profession: 'actor',
+    children: ['Fred', 'Anna', 'Max', 'Alex'],
+  },
+};
+//bad practic
+console.log(user.public1);
+console.log(user.privateInfo.login);
+console.log(user.privateInfo.bd.year);
 
-console.log(set1);
+//bad practic without деструктуризація
+// const public1 = user.public1;
+// const login = user.privateInfo.login;
+// const year = user.privateInfo.bd.year;
 
-console.log(...set1.entries());
-console.log(...set1.keys());
-console.log(...set1.values());
+//good practice with деструктуризація
+const {
+  public1,
+  privateInfo: {
+    login,
+    bd: { year, month, day },
+  },
+  //дістати телефон з новим ім'ям змінної userPhone
+  contactInfo: { phone: userPhone },
+  //дістати ім'я третьої дитини з ім'ям змінної thirdChild
+  generalInfo: {
+    children: [, , thirdChild],
+  },
+} = user;
 
-const sent1 = 'Set.prototype.union()';
-const letters = new Set('qwqqq'); //q,w,q,q,q
-console.log(letters);
-
-const sent2 = 'to be or not to be';
-const words = new Set(sent2.split(' '));
-console.log(words);
+console.log(public1);
+console.log(login);
+console.log(year);
+console.log(month);
+console.log(userPhone);
+console.log(thirdChild);
